6c8932bb3b691aaae5040eae94a20dc51a7ee764
VeeSimz <kayveesimba@gmail.com>
2023-09-16 21:45:36 +0200

    Added switch cases to specifier c and s

---
+++ 0-printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* _printf - print clone
+* @format: constant string literal
+* Return: 0 Success
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list check_list;
+	int i, j = 0;
+	char c, *str;
+
+	va_start(check_list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+
+			i++;
+			switch (format[i])
+			{
+			case 'c':
+				c = (char) va_arg(check_list, int);
+				_putchar(c);
+				break;
+			case 's':
+				str = va_arg(check_list, char *);
+
+				while (str[j] != '\0')
+				{
+					_putchar(*str);
+					str++;
+				}
+				break;
+			}
+		}
+
+		else
+		{
+			char c = format[i];
+			_putchar(c);
+		}
+		}
+	va_end(check_list);
+	_putchar('\n');
+	return (0);
+
+}
