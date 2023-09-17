7134bc3ecb7480329e58e27d1bdfbde612e085c4
VeeSimz <kayveesimba@gmail.com>
2023-09-17 12:20:26 +0200

    Add error handling

--- 0-printf.c
+++ 0-printf.c
@@ -10,7 +10,9 @@ int _printf(const char *format, ...)
 {
 	va_list check_list;
 	int i, j = 0;
+	int index = 0, error = 0;
 	char c, *str;
+	char errormsg[] = "Invalid format or argument";
 
 	va_start(check_list, format);
 	for (i = 0; format[i] != '\0'; i++)
@@ -26,13 +28,25 @@ int _printf(const char *format, ...)
 				_putchar(c);
 				break;
 			case 's':
-				str = va_arg(check_list, char *);
-
-				while (str[j] != '\0')
 				{
-					_putchar(*str);
-					str++;
+					str = va_arg(check_list, char *);
+
+					if (str == NULL)
+					{
+						error = 1;
+					}
+					else
+					{
+						while (str[j] != '\0')
+						{
+							_putchar(*str);
+							str++;
+						}
+					}
+					break;
 				}
+			default:
+				error = 1;
 				break;
 			}		
 		}
@@ -45,6 +59,14 @@ int _printf(const char *format, ...)
 		}
 		}
 	va_end(check_list);
+	if (error == 1)
+	{
+		while (errormsg[index] != '\0')
+		{
+			_putchar(errormsg[index]);
+			index++;
+		}
+	}
 	/*_putchar('\n');8*/
 	return (0);
 
